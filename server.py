4f3614c208bfed9d2a8e32281456a09a46f9055c
Rinard Akhmetzyanov <rinardahetzyanov@yandex.ru>
2025-04-20 15:54:38 -0700

    Результат отбора

--- server.py
+++ server.py
@@ -1,58 +1,27 @@
-from flask import Flask, render_template_string
+from flask import Flask
 
 app = Flask(__name__)
-
-planets_info = {
-    "Марс": [
-        "Эта планета близка к Земле;",
-        "На ней много необходимых ресурсов;",
-        "На ней есть вода и атмосфера;",
-        "На ней есть небольшое магнитное поле;",
-        "Наконец, она просто красивая!"
-    ],
-    "Венера": [
-        "Очень горячая планета;",
-        "Имеет толстую атмосферу;",
-        "Богата углеродом и серой;",
-        "Потенциально может быть преобразована;"
-    ],
-    "Юпитер": [
-        "Самая большая планета Солнечной системы;",
-        "Имеет множество спутников;",
-        "Обладает мощным магнитным полем;",
-        "Текущие технологии не позволяют освоить ее;"
-    ]
-}
-
-html_tem = """
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Выбор планеты</title>
-    <!-- Подключение Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body>
-    <div class="container mt-5">
-        <h1>Мое предложение: {{ planet_name }}</h1>
-        {% for info in planet_info %}
-            {% set color_classes = ['alert-success', 'alert-warning', 'alert-danger', 'alert-info'] %}
-            <div class="alert {{ color_classes[loop.index0 % color_classes|length] }}" role="alert">{{ info }}</div>
-        {% endfor %}
-    </div>
-</body>
-</html>
-"""
-
-@app.route('/choice/<planet_name>')
-def choice(planet_name):
-    if planet_name in planets_info:
-        planet_info = planets_info[planet_name]
-        return render_template_string(html_tem, planet_name=planet_name, planet_info=planet_info)
-    else:
-        return "Планета не найдена", 404
+@app.route('/results/<nickname>/<int:level>/<float:rating>')
+def two_params(nickname, level, rating):
+    return f'''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet"
+                   href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                   integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                   crossorigin="anonymous">
+                    <title>Результат отбора</title>
+                  </head>
+                  <body>
+                    <h2>Результаты отбора</h2>
+                    <div>Претендента на участие в мисии {nickname}</div>
+                    <div class="alert alert-success" role="alert">Поздравляем ваш рейтинг после {level} этапа отбора</div>
+                    <div>состовляет {rating}</div>
+                    <div class="alert alert-warning" role="alert">Желаем удачи</div>
+                  </body>
+                </html>'''
 
 if __name__ == '__main__':
     app.run(debug=True)
