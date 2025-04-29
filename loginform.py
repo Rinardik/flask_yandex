e193f743bd061b810991aa7a91740ae29c995998
Rinard Akhmetzyanov <rinardahetzyanov@yandex.ru>
2025-04-29 07:59:53 -0700

    По каютам!

--- loginform.py
+++ loginform.py
@@ -1,26 +1,14 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
-from flask import redirect, render_template, Flask
-
-
-class LoginForm(FlaskForm):
-    id_astr = StringField('id астронавта', validators=[DataRequired()])
-    password_astr = PasswordField('Пароль астронавта', validators=[DataRequired()])
-    id_kap = StringField('id астронавта', validators=[DataRequired()])
-    password_kap = PasswordField('Пароль астронавта', validators=[DataRequired()])
-    submit = SubmitField('Доступ')
+from flask import render_template, Flask
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        return redirect('/success')
-    return render_template('login.html', title='Авторизация', form=form)
+@app.route('/distribution')
+def distribution():
+    astronauts = ['Иванов И.И.', 'Петров П.П.', 'Сидоров С.С.', 'Козлов К.К.']
+    return render_template('distribution.html', astronauts=astronauts)
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
