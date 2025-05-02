bccd0ba64e70add18d7528a1a58748bb046c6b04
Rinard Akhmetzyanov <rinardahetzyanov@yandex.ru>
2025-05-02 14:41:45 -0700

    Цвет каюты

--- server.py
+++ server.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
  
 app = Flask(__name__)
 
-@app.route('/training/<prof>')
-def training(prof):
-    return render_template('training.html', prof=prof)
+@app.route('/table')
+def table():
+    gender = request.args.get('gender')
+    age_str = request.args.get('age')
+    return render_template('training.html', gender=gender, age=int(age_str))
 
 
 if __name__ == '__main__':
