40860c8a92e06f874cdf2a0ebb975580100ebeb4
Rinard Akhmetzyanov <rinardahetzyanov@yandex.ru>
2025-05-04 12:41:40 -0700

    Журнал работ

--- data/jobs.py
+++ data/jobs.py
@@ -1,17 +1,20 @@
-import datetime
 import sqlalchemy
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
 from .db_session import SqlAlchemyBase
+from .users import User
 
 
 class Jobs(SqlAlchemyBase):
-    __tablename__ = 'users'
-    id = sqlalchemy.Column(sqlalchemy.Integer, 
-                           primary_key=True, autoincrement=True)
-    team_leader = sqlalchemy.Column(sqlalchemy.Integer)
-    job = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    work_size = sqlalchemy.Column(sqlalchemy.Integer)
-    collaborators = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    created_date = sqlalchemy.Column(sqlalchemy.String)
-    start_date = sqlalchemy.Column(sqlalchemy.DateTime)
-    end_date = sqlalchemy.Column(sqlalchemy.DateTime)
-    is_finished = sqlalchemy.Column(sqlalchemy.Boolean)
+    __tablename__ = 'jobs'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    team_leader = Column(Integer, ForeignKey('users.id'))
+    job = Column(String, nullable=True)
+    work_size = Column(Integer)
+    collaborators = Column(String, nullable=True)
+    created_date = Column(String)
+    start_date = Column(DateTime)
+    end_date = Column(DateTime)
+    is_finished = Column(Boolean)
+    leader = relationship("User")
--- main.py
+++ main.py
@@ -1,44 +1,20 @@
-from flask import Flask, render_template, redirect
-from werkzeug.security import generate_password_hash, check_password_hash
-from data.users import User
-from data import db_session
-from forms.user import RegisterForm
+from flask import Flask, render_template
+import os
+from data.db_session import global_init, create_session
+from data.jobs import Jobs
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
-
-def set_password(self, password):
-    self.hashed_password = generate_password_hash(password)
-
-def check_password(self, password):
-    return check_password_hash(self.hashed_password, password)
+db_path = os.path.join(os.getcwd(), 'db', 'blogs.db')
+global_init(db_path)
 
 
-@app.route('/register', methods=['GET', 'POST'])
-def reqister():
-    form = RegisterForm()
-    if form.validate_on_submit():
-        if form.password.data != form.password_again.data:
-            return render_template('register.html', title='Регистрация',
-                                   form=form,
-                                   message="Пароли не совпадают")
-        db_sess = db_session.create_session()
-        if db_sess.query(User).filter(User.email == form.email.data).first():
-            return render_template('register.html', title='Регистрация',
-                                   form=form,
-                                   message="Такой пользователь уже есть")
-        user = User(
-            name=form.name.data,
-            email=form.email.data,
-            about=form.about.data
-        )
-        user.set_password(form.password.data)
-        db_sess.add(user)
-        db_sess.commit()
-        return redirect('/login')
-    return render_template('register.html', title='Регистрация', form=form)
+@app.route('/jobs')
+def jobs_list():
+    session = create_session()
+    jobs = session.query(Jobs).all()
+    return render_template('journal.html', jobs=jobs)
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    app.run(debug=True)
