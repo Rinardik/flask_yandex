f087e99cf763ccd19a15e77b9d87d868506f0302
Rinard Akhmetzyanov <rinardahetzyanov@yandex.ru>
2025-05-06 15:42:50 -0700

    Форма регистрации

---
+++ forms/user.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, TextAreaField, SubmitField, EmailField, IntegerField
+from wtforms.validators import DataRequired
+
+
+class RegisterForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
+    surname = StringField('Фамилия', validators=[DataRequired()])
+    name = StringField('Имя', validators=[DataRequired()])
+    age = IntegerField('Возраст', validators=[DataRequired()])
+    position = StringField('Должность', validators=[DataRequired()])
+    speciality = StringField('Специальность', validators=[DataRequired()])
+    address = StringField('Адрес', validators=[DataRequired()])
+    submit = SubmitField('Зарегистрироваться')
+ 
--- server.py
+++ server.py
@@ -7,13 +7,18 @@ import json
 import random
 from data.db_session import global_init, create_session
 from data.jobs import Jobs
+from data.users import User
+from forms.user import RegisterForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 app.config['UPLOAD_FOLDER'] = 'static/uploads'
 os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 CREW_JSON_PATH = os.path.join(app.root_path, 'templates', 'astr.json')
-db_path = os.path.join(os.getcwd(), 'db', 'blog.db')
+base_dir = os.path.dirname(os.path.abspath(__file__))
+db_dir = os.path.join(base_dir, 'db')
+os.makedirs(db_dir, exist_ok=True)
+db_path = os.path.join(db_dir, 'blog.db')
 global_init(db_path)
 
 def get_random_crew_member():
@@ -28,6 +33,41 @@ class LoginForm(FlaskForm):
     id_kap = StringField('id астронавта', validators=[DataRequired()])
     password_kap = PasswordField('Пароль астронавта', validators=[DataRequired()])
     submit = SubmitField('Доступ')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template(
+                'register.html',
+                title='Регистрация',
+                form=form,
+                message="Пароли не совпадают"
+            )
+        db_sess = create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template(
+                'register.html',
+                title='Регистрация',
+                form=form,
+                message="Такой пользователь уже существует"
+            )
+
+        user = User()
+        user.surname = form.surname.data
+        user.name = form.name.data
+        user.age = form.age.data
+        user.position = form.position.data
+        user.speciality = form.speciality.data
+        user.address = form.address.data
+        user.email = form.email.data
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect(url_for('success'))
+    return render_template('register.html', title='Регистрация', form=form)
 
 
 @app.route('/member')
@@ -234,6 +274,11 @@ def jobs_list():
     session = create_session()
     jobs = session.query(Jobs).all()
     return render_template('journal.html', jobs=jobs)
+
+
+@app.route('/success')
+def success():
+    return "Регистрация успешна!"
 
 
 if __name__ == '__main__':
